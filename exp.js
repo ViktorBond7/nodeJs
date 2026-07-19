14a5a05b9c24fb70069c73e38e27beab7b4a60bb
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2026-07-19 10:51:27 +0200

    use docker

---
+++ exp.js
@@ -0,0 +1,225 @@
+// function memoize(fn) {
+//   // Твій код тут (використовуй замикання для збереження об'єкта cache)
+//   const cache = new Map();
+
+//   return function (x) {
+//     if (cache.has(x)) {
+//       return cache.get(x);
+//     }
+//     const res = fn(x);
+//     cache.set(x, res);
+//     return res;
+//   };
+// }
+
+// // Приклад для тестування:
+// const slowDouble = (x) => {
+//   console.log("Рахую...");
+//   return x * 2;
+// };
+
+// const fastDouble = memoize(slowDouble);
+
+// console.log(fastDouble(5)); // Виведе: "Рахую..." потім 10
+// console.log(fastDouble(5)); // Виведе: 10 (миттєво, без "Рахую...")
+// console.log(fastDouble(10)); // Виведе: "Рахую..." потім 20
+/////////////////////////////////////////////
+////////////////////////////////////////////
+// function debounce(recipeFn, delay) {
+//   // Твій код тут (знадобляться setTimeout та clearTimeout)
+//   let timer;
+//   return function () {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       recipeFn(...arguments);
+//     }, delay);
+//   };
+// }
+
+// // Тест:
+// const processSearch = (query) => console.log(`Шукаю: ${query}`);
+// const debouncedSearch = debounce(processSearch, 500);
+
+// // Імітуємо швидке введення тексту користувачем:
+// debouncedSearch("j");
+// debouncedSearch("ja");
+// debouncedSearch("jav");
+// debouncedSearch("javas");
+// // Лише цей виклик має спрацювати через 500 мс після останнього кліку:
+// debouncedSearch("javascript");
+//////////////////////////
+////////////////////////////
+
+// const url = [
+//   "https://jsonplaceholder.typicode.com/users/",
+//   "https://jsonplaceholder.typicode.com/posts?userId=",
+//   "https://jsonplaceholder.typicode.com/todos?userId=",
+// ];
+
+// const getInfoUser = async (id) => {
+//   const res = {};
+//   const dataUser = [];
+//   try {
+//     for (const el of url) {
+//       const res = await fetch(`${el}${id}`);
+//       dataUser.push(await res.json());
+//     }
+
+//     await Promise.all([...dataUser]).then((v) => {
+//       ((res.profile = v[0]), (res.posts = v[1]), (res.todos = v[2]));
+//     });
+//     return res;
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+// console.log(getInfoUser(1).then((v) => console.log(v)));
+
+// const url = [
+//   "https://jsonplaceholder.typicode.com/users/",
+//   "https://jsonplaceholder.typicode.com/posts?userId=",
+//   "https://jsonplaceholder.typicode.com/todos?userId=",
+// ];
+
+// const getInfoUser = async (id) => {
+//   try {
+//     const fetchProm = url.map((endpoind) => fetch(`${endpoind}${id}`));
+//     const res = await Promise.all(fetchProm);
+
+//     for (const el of res) {
+//       if (!el.ok) throw new Error(`Response status: ${response.status}`);
+//     }
+
+//     const [aaaaa, bbbbbb, cccccccc] = await Promise.all(
+//       res.map((i) => i.json()),
+//     );
+//     return { aaaaa, bbbbbb, cccccccc };
+
+//     // console.log(res);
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// };
+
+// // Тест
+// getInfoUser(1)
+//   .then((data) => console.log("Успіх! Отримані дані:", data))
+//   .catch((err) => console.log("Помилка в тесті:", err));
+// ..................
+
+// function deepClone(obj) {
+//   if (obj === null || typeof obj !== "object") {
+//     return obj;
+//   }
+
+//   const clone = Array.isArray(obj) ? [] : {};
+
+//   for (const key in obj) {
+//     // if (!Object.hasOwn(object, key)) continue;
+
+//     clone[key] = deepClone(obj[key]);
+//   }
+//   return clone;
+// }
+// const original = {
+//   name: "Олексій",
+//   skills: ["JS", "React"],
+//   address: {
+//     city: "Київ",
+//     coordinates: { lat: 50.45, lng: 30.52 },
+//   },
+// };
+// // deepClone(original);
+// const clone = deepClone(original);
+// clone.address.coordinates.lat = 49.0; // Змінюємо координату в клоні
+// clone.skills.push("Node.js"); // Змінюємо масив у клоні
+
+// console.log(original.address.coordinates.lat); // Має залишитися 50.45!
+// console.log(original.skills); // Має залишитися ["JS", "React"] (без Node.js)
+
+//////////////////////////////////////
+
+// function throttle(recipeFn, limit) {
+//   // Твій код тут (використовуй прапорець-запобіжник або обчислення часу через Date.now())
+//   // const time = Date.now();
+//   let isBloc = false;
+
+//   return function (...args) {
+//     if (isBloc) return;
+//     const d = 5;
+//     recipeFn(...args);
+//     isBloc = true;
+
+//     setTimeout(() => {
+//       isBloc = false;
+//     }, limit);
+//   };
+// }
+
+// // Тест:
+// const logClick = (g) => console.log("Клік зафіксовано!", g);
+// const throttledClick = throttle(logClick, 1000);
+
+// // Імітуємо швидкі кліки що-100 мілісекунд:
+// const interval = setInterval(throttledClick, 100);
+
+// // Через 3.5 секунди зупинимо тест.
+// // За цей час (3500 мс) з лімітом в 1000 мс функція має вивести напис у консоль рівно 4 рази.
+// setTimeout(() => clearInterval(interval), 3500);
+// ...............................
+
+// Напиши власну реалізацію стандартного методу Function.prototype.bind
+// під назвою customBind.Твоя функція має додаватися до прототипу всіх функцій,
+//   приймати контекст(context) та будь - яку кількість початкових аргументів(каррирування),
+//     а потім повертати нову функцію, яку також можна викликати з додатковими аргументами.
+
+// Function.prototype.customBind = function (context, ...args) {
+//   // Твій код тут
+//   // Підказка: this всередині цієї функції вказує на саму функцію, яку ми біндимо
+//   const originalFn = this;
+//   return function (...newArgs) {
+//     return originalFn.apply(context, [...args, ...newArgs]);
+//   };
+// };
+// Function.prototype.customBind = function (context, ...args) {
+//   const originalFn = this;
+//   return function (...newArgs) {
+//     // Робимо те саме, але розгортаємо аргументи через кому всередині .call
+//     return originalFn.call(context, ...args, ...newArgs);
+//   };
+// };
+// // Тест:
+// const user = {
+//   name: "Марія",
+// };
+
+// function greet(greeting, punctuation) {
+//   return `${greeting}, ${this.name}${punctuation}`;
+// }
+
+// // Прив'язуємо контекст "user" та перший аргумент "Привіт"
+// const greetMaria = greet.customBind(user, "Привіт");
+
+// // Викликаємо нову функцію і передаємо залишок аргументів
+// console.log(greetMaria("!")); // Має вивести: "Привіт, Марія!"
+
+const arr = [
+  { name: "Alex", age: 20, isActive: false },
+  { name: "Bro", age: 20, isActive: false },
+  { name: "Jon", age: 20, isActive: false },
+];
+
+// const foo = arr.filter((item) => item.isActive);
+// console.log(foo);
+
+Array.prototype.castomReduce = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    // console.log("this", this);
+    if (cb) return true;
+  }
+};
+
+// Array.prototype.castomFilter = castomFilter;
+const foo = arr.some((item) => item.isActive);
+console.log(foo);
--- express-server/app.ts
+++ express-server/app.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -7,72 +8,22 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Віддаємо статичні файли
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.urlencoded({ extended: true }));
 
-// Middleware для парсингу даних форм
-app.use(express.urlencoded({ extended: true }));
+// // Налаштування multer
+// const upload = multer({ dest: "uploads/" });
 
-interface User {
-  username: string;
-  email: string;
-  age: number;
-}
+// app.post("/upload", upload.single("avatar"), (req: Request, res: Response) => {
+//   console.log("Текстові дані:", req.body);
+//   console.log("Файл:", req.file);
+//   res.send("Файл завантажено!");
+// });
+const PORT = process.env.PORT ?? 3000;
 
-interface RegisterBody {
-  username: string;
-  email: string;
-  age: string;
-}
-
-// Масив для зберігання користувачів
-const users: User[] = [];
-
-// Маршрут для головної сторінки
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.json({ message: "Hello from Docker!" });
 });
-
-// Маршрут для сторінки реєстрації
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "register.html"));
-});
-
-// Обробник POST запиту
-app.post("/register", (req, res) => {
-  const body = req.body as RegisterBody;
-  console.log(body);
-
-  const user: User = {
-    username: req.body.username,
-    email: req.body.email,
-    age: Number(req.body.age),
-  };
-
-  users.push(user);
-  console.log("Всього користувачів:", users.length);
-
-  res.send("Реєстрацію завершено!");
-});
-
-
-// Маршрут для сторінки пошуку
-app.get('/search', (req, res) => {
-  const { q } = req.query as { q: string }
-
-  if (!q) {
-    return res.sendFile(path.join(__dirname, 'public', 'search.html'))
-  }
-
-  const results = users.filter((user) =>
-    user.username.toLowerCase().includes(q.toLowerCase()),
-  )
-
-  res.send(`Знайдено користувачів: ${results.length}`)
-})
-
-
-
-app.listen(3000, () => {
-  console.log("Server is running on <http://localhost:3000/register>");
+app.listen(PORT, () => {
+  console.log(`Server is running on <http://localhost:${PORT}>`);
 });
